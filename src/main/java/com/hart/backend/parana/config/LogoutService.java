3419f0b5fdc68c3900ab0a41ee59bd5e367dadc9
Ian Hart <ianhart7790@gmail.com>
2023-10-11 16:07:00 -0400

    feat: user can now logout

---
+++ src/main/java/com/hart/backend/parana/config/LogoutService.java
@@ -0,0 +1,57 @@
+package com.hart.backend.parana.config;
+
+import com.hart.backend.parana.advice.NotFoundException;
+import com.hart.backend.parana.refreshtoken.RefreshTokenService;
+import com.hart.backend.parana.token.TokenRepository;
+import com.hart.backend.parana.user.User;
+import com.hart.backend.parana.user.UserRepository;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Service
+public class LogoutService implements LogoutHandler {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final RefreshTokenService refreshTokenService;
+
+    public LogoutService(JwtService jwtService,
+            UserRepository userRepository,
+            TokenRepository tokenRepository,
+            RefreshTokenService refreshTokenService) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+        this.tokenRepository = tokenRepository;
+        this.refreshTokenService = refreshTokenService;
+    }
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        String authHeader = request.getHeader("Authorization");
+        String jwt;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+
+        var storedToken = this.tokenRepository.findByToken(jwt).orElse(null);
+        User user = this.userRepository.findByEmail(this.jwtService.extractUsername(jwt))
+                .orElseThrow(() -> new NotFoundException("User not found logging out."));
+
+        storedToken.setRevoked(true);
+        storedToken.setExpired(true);
+        this.tokenRepository.save(storedToken);
+        this.refreshTokenService.revokeAllUserRefreshTokens(user);
+        SecurityContextHolder.clearContext();
+
+    }
+}
