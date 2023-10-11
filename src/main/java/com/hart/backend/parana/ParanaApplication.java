c760ffcf3554feb7ecc777e96e9494ee33a00da3
Ian Hart <ianhart7790@gmail.com>
2023-10-11 10:14:55 -0400

    feat: initial commit

---
+++ src/main/java/com/hart/backend/parana/ParanaApplication.java
@@ -0,0 +1,13 @@
+package com.hart.backend.parana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hart/backend/parana/ParanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hart.backend.parana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
