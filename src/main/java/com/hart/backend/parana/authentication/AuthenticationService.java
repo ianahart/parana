f510a88a86028cef7e4df4ea996ffe99b60b89f4
Ian Hart <ianhart7790@gmail.com>
2023-10-11 15:46:53 -0400

    feat: user can now authenticate/login

--- src/main/java/com/hart/backend/parana/authentication/AuthenticationService.java
+++ src/main/java/com/hart/backend/parana/authentication/AuthenticationService.java
@@ -2,23 +2,33 @@ package com.hart.backend.parana.authentication;
 
 import java.util.Optional;
 
+import com.hart.backend.parana.authentication.request.LoginRequest;
 import com.hart.backend.parana.authentication.request.RegisterRequest;
+import com.hart.backend.parana.authentication.response.LoginResponse;
 import com.hart.backend.parana.authentication.response.RegisterResponse;
 import com.hart.backend.parana.config.JwtService;
 import com.hart.backend.parana.profile.ProfileService;
+import com.hart.backend.parana.refreshtoken.RefreshToken;
 import com.hart.backend.parana.refreshtoken.RefreshTokenRepository;
 import com.hart.backend.parana.refreshtoken.RefreshTokenService;
+import com.hart.backend.parana.token.Token;
 import com.hart.backend.parana.token.TokenRepository;
 import com.hart.backend.parana.token.TokenService;
+import com.hart.backend.parana.token.TokenType;
 import com.hart.backend.parana.user.Role;
 import com.hart.backend.parana.user.User;
 import com.hart.backend.parana.user.UserRepository;
 import com.hart.backend.parana.user.UserService;
+import com.hart.backend.parana.user.dto.UserDto;
 import com.hart.backend.parana.util.MyUtil;
 import com.hart.backend.parana.advice.BadRequestException;
+import com.hart.backend.parana.advice.ForbiddenException;
+import com.hart.backend.parana.advice.NotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -85,9 +95,60 @@ public class AuthenticationService {
                 String.format("%s %s", firstName, lastName),
                 request.getRole().equals("USER") ? Role.USER : Role.TEACHER,
                 false,
-                this.profileService.createProfile());
+                this.profileService.createProfile(),
+                this.passwordEncoder.encode(request.getPassword()));
         this.userRepository.save(user);
         return new RegisterResponse("User created");
+    }
+
+    public void saveTokenWithUser(String token, User user) {
+        Token tokenToSave = new Token(token, TokenType.BEARER, false, false, user);
+        this.tokenRepository.save(tokenToSave);
+
+    }
+
+    private UserDto updateAuthUser(User user, String jwtToken) {
+
+        user.setLoggedIn(true);
+
+        this.userRepository.save(user);
+        this.saveTokenWithUser(jwtToken, user);
+
+        return new UserDto(
+                user.getId(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getRole(),
+                user.getAbbreviation(),
+                user.getLoggedIn(),
+                user.getProfile().getId(),
+                user.getProfile().getAvatarUrl());
+
+    }
+
+    public LoginResponse login(LoginRequest request) {
+
+        try {
+            this.authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(),
+                            request.getPassword()));
+
+        } catch (BadCredentialsException e) {
+            System.out.println(e);
+            throw new ForbiddenException("Credentials are invalid");
+        }
+        User user = this.userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new NotFoundException("User not found by email."));
+
+        String jwtToken = this.jwtService.generateToken(user);
+
+        this.tokenService.revokeAllUserTokens(user);
+        UserDto userDto = this.updateAuthUser(user, jwtToken);
+        RefreshToken refreshToken = this.refreshTokenService.generateRefreshToken(user.getId());
+
+        return new LoginResponse(userDto, jwtToken, refreshToken.getRefreshToken());
     }
 
 }
