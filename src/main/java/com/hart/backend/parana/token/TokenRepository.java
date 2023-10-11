d72a63696c6d99c5340a37c512ea5237bd907683
Ian Hart <ianhart7790@gmail.com>
2023-10-11 12:30:53 -0400

    feat: add token class, service, and repository

---
+++ src/main/java/com/hart/backend/parana/token/TokenRepository.java
@@ -0,0 +1,29 @@
+package com.hart.backend.parana.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    @Query(value = """
+             SELECT expired, t.id, revoked, token, token_type, user_id
+             FROM token t
+             INNER JOIN _user u
+             ON t.user_id = u.id
+             WHERE T.user_id = :id
+             AND t.expired = false
+            OR t.revoked = false
+                            """, nativeQuery = true)
+    List<Token> findAllValidTokens(@Param("id") Long id);
+
+    Optional<Token> findByToken(String token);
+
+}
---
+++ src/main/java/com/hart/backend/parana/token/TokenService.java
@@ -0,0 +1,35 @@
+package com.hart.backend.parana.token;
+
+import java.util.List;
+
+import com.hart.backend.parana.user.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+    private final TokenRepository tokenRepository;
+
+    @Autowired
+    public TokenService(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    public void revokeAllUserTokens(User user) {
+        List<Token> tokens = this.tokenRepository.findAllValidTokens(user.getId());
+
+        if (tokens.isEmpty()) {
+            return;
+        }
+
+        tokens.forEach(t -> {
+            t.setExpired(true);
+            t.setRevoked(true);
+        });
+
+        this.tokenRepository.saveAll(tokens);
+    }
+
+}
