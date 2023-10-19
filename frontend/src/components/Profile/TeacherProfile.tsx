7370b450906c4c67c0d71f75c929a6e40f96ca80
Ian Hart <ianhart7790@gmail.com>
2023-10-19 19:30:04 -0400

    feat: add first lesson free to teacher profile

--- frontend/src/components/Profile/TeacherProfile.tsx
+++ frontend/src/components/Profile/TeacherProfile.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import { IProfile } from '../../interfaces';
 import Terrain from './Terrain';
 import Biography from './Biography';
@@ -6,12 +6,14 @@ import LessonLocation from './LessonLocation';
 import AboutLesson from './AboutLesson';
 import Tags from './Tags';
 import TeacherHero from './TeacherHero';
+import { IoRibbonOutline } from 'react-icons/io5';
 
 interface ITeacherProfileProps {
   profile: IProfile;
 }
 
 const TeacherProfile = ({ profile }: ITeacherProfileProps) => {
+  console.log(profile);
   return (
     <Flex className="teacher-profile-container" color="text.secondary">
       <Flex p="0.5rem" flexDir="column" flexGrow={2} minH="100vh">
@@ -23,6 +25,24 @@ const TeacherProfile = ({ profile }: ITeacherProfileProps) => {
         />
         <Biography firstName={profile.firstName} bio={profile.bio} />
         <AboutLesson level="All Levels" lang="English" about={profile.aboutLesson} />
+        {profile.firstLessonFree && (
+          <Box my="1rem" width="200px">
+            <Flex
+              align="center"
+              py="0.45rem"
+              px="1rem"
+              borderRadius={20}
+              fontWeight="bold"
+              bg="rgba(43, 67, 101, 0.5)"
+              m="0.5rem"
+            >
+              <Box mx="0.25rem">
+                <IoRibbonOutline />
+              </Box>
+              <Text fontWeight="bold">First lesson free</Text>
+            </Flex>
+          </Box>
+        )}
         <Tags tags={profile.tags} heading="Hashtags" />
       </Flex>
       <Box
