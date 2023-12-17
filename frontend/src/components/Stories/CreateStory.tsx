82e91f7dc995ff042ff07ae5b1dee0d14ab4fe22
Ian Hart <ianhart7790@gmail.com>
2023-12-17 17:14:19 -0500

    fix: change sock url from localhost to production link

--- frontend/src/components/Stories/CreateStory.tsx
+++ frontend/src/components/Stories/CreateStory.tsx
@@ -33,7 +33,7 @@ const CreateStory = () => {
   }, []);
 
   const connect = () => {
-    let Sock = new SockJS('http://localhost:8080/ws');
+    let Sock = new SockJS('https://parana-hart-6c0dd51d52f9.herokuapp.com/wss');
     stompClient = over(Sock);
     stompClient.connect({}, onConnected, onError);
   };
